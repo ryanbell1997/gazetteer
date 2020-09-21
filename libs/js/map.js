7c223aa7036a91c56b97b2ff776fcd43b7c83196
Ryan Bell <ryan.bell1997@hotmail.co.uk>
2020-09-21 14:42:37 +0100

    Working on internet for now and returning info.

--- libs/js/map.js
+++ libs/js/map.js
@@ -1,8 +1,11 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=NHZ05sbIiAjl55cMhy0p', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-}).addTo(map);
+L.tileLayer('https://{s}.tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=2e011da1ea684120b8c6bf9fa2df5a98', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: '2e011da1ea684120b8c6bf9fa2df5a98',
+	maxZoom: 22
+},
+).addTo(map);
 
 function setLJSON(storageName,item) {
     localStorage.setItem(storageName, JSON.stringify(item));
@@ -18,7 +21,7 @@ function getInitialLocation(){
             navigator.geolocation.getCurrentPosition((position) => {
                 $(document).ready(() => {
                     $.ajax({
-                        url: '../libs/php/getCountry.php',
+                        url: './libs/php/getCountry.php',
                         type: 'POST',
                         dataType: 'json',
                         data: {
@@ -27,10 +30,11 @@ function getInitialLocation(){
                         },
                         success: (result) => {
                             if(result.status.name == "ok"){
-                                const currentCountryName = result['data'][0]['components']['country'];
+                                const currentCountryName = result['data']['results'][0]['components']['country'];
                                 const currentLatLng = [position.coords.latitude, position.coords.longitude];
                                 map.setView([position.coords.latitude, position.coords.longitude]);
-                                L.marker([position.coords.latitude, position.coords.longitude]).addTo(map).bindPopup(`<b>${currentCountryName}!</b><br>`).openPopup();
+                                map.setZoom(6);
+                                L.marker([position.coords.latitude, position.coords.longitude]).addTo(map).bindPopup(`<b>${currentCountryName}</b><br>`).openPopup();
                                 setLJSON('initialLocation', currentCountryName);
                                 setLJSON('initialCoords', currentLatLng);
                                 setLJSON('initialCountry', currentCountryName);
@@ -47,6 +51,7 @@ function getInitialLocation(){
     } else {
         var coords = getLJSON('initialCoords');
         map.setView([coords[0], coords[1]]);
+        map.setZoom(6)
         L.marker([coords[0], coords[1]]).addTo(map).bindPopup(`<b>${getLJSON('initialCountry')}!</b><br>`).openPopup();
     }     
 }
@@ -54,7 +59,7 @@ function getInitialLocation(){
 function returnCountryInformation() {
     $(document).ready(() => {
         $.ajax({
-            url: '../libs/php/searchProcessor.php',
+            url: './libs/php/searchProcessor.php',
             type: 'POST',
             dataType: 'json',
             data: {
@@ -62,17 +67,25 @@ function returnCountryInformation() {
             },
             success: (result) => {
                 map.setView([result['data'][0]['latlng'][0], result['data'][0]['latlng'][1]]);
-                L.marker([result['data'][0]['latlng'][0], result['data'][0]['latlng'][1]]).addTo(map).bindPopup(`<b>${result['data'][0]['name']}!</b><br>
+                map.setZoom(6);
+                L.marker([result['data'][0]['latlng'][0], result['data'][0]['latlng'][1]]).addTo(map).bindPopup(`<b>${result['data'][0]['name']}</b><br>
                 Capital: ${result['data'][0]['capital']}<br>
                 Region: ${result['data'][0]['region']}<br>
                 Population: ${result['data'][0]['population']}<br>
                 Currency: ${result['data'][0]['currencies'][0]['name']}<br>
+                <br>
+                <a href="#">See more info</a><br>
                 `).openPopup();
 
             }
         })
     })
 }
+/*
+$(document).ready(() => {
+    getInitialLocation();
+})
+*/
 
 $(document).ready(() => {
     getInitialLocation();
@@ -81,6 +94,12 @@ $(document).ready(() => {
 $('#countrySearch').on('click', () => {
     returnCountryInformation();
 }) 
+//Get enter press working in order to search.
+$('#countryInput').on('keyup', (e) => {
+    if(e.which == 13) {
+        returnCountryInformation();
+    }
+})
 
 
 
